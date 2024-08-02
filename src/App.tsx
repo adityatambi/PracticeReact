6dc25da7b2eccd70f38271cae378cbbb9170cabb
adityatambi <adityatambi10@gmail.com>
2024-08-02 21:47:29 +0530

    person score editor using useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { PersonScore } from './personScore';
+import { PersonScoreuseReducer } from './personScoreuseReducer';
 
 function App() {
   return (
     <div className="App">
       <PersonScore />
+      <PersonScoreuseReducer />
     </div>
   );
 }
