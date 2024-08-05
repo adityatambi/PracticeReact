cbf83033433de56df75f11f2a02b8a8ab735583e
adityatambi <adityatambi10@gmail.com>
2024-08-05 21:56:59 +0530

    practice useMemo to increase efficiency

--- src/personScore.tsx
+++ src/personScore.tsx
@@ -1,10 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { getPerson } from './getPerson';
 
 export function PersonScore() {
   const [name, setName] = useState<string | undefined>();
   const [score, setScore] = useState(0);
   const [loading, setLoading] = useState(true);
+
+  function sillyExpensiveFunction() {
+    console.log('Executing silly expensive function');
+
+    let sum = 0;
+    for (let i = 0; i < 10000; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 
   useEffect(() => {
     getPerson().then((person) => {
@@ -13,6 +23,7 @@ export function PersonScore() {
       console.log('state value', loading, name, score);
     });
   }, [name, score]);
+  const sillyExpensiveCalculation = useMemo(() => sillyExpensiveFunction(), []);
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -21,6 +32,7 @@ export function PersonScore() {
       <h3>
         {name}, {score}
       </h3>
+      <p>{sillyExpensiveCalculation}</p>
       <button onClick={() => setScore(score + 1)}>Add</button>
       <button onClick={() => setScore(score - 1)}>Subtract</button>
       <button onClick={() => setScore(0)}>Reset</button>
